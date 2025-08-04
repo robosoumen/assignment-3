f2344815ca0ab815f8cd6defeb79c5f059a4d87d
soumen bera <soumenbera211@gmail.com>
2025-08-04 22:43:28 +0530

    first commit

---
+++ problem-3.js
@@ -0,0 +1,21 @@
+// মাসের ১ তারিখে রহিমা বেগম কে ডাক্তার কিছু ঔষধ দিয়েছেন  যেটা ঠিক ৩  দিন পর পর খেতে হবে। রহিমা বেগম কে যতদিন ঔষধ খেতে হবে সেটা lastDay নামে একটা ভ্যারিয়েবলে সেইভ করা আছে। 
+// রহিমা বেগম কোন কোন দিন ঔষধ খাবে এবং কোন কোন দিন ঔষধ খাবেনা  তার একটা লিস্ট তোমাকে প্রোগ্রামিং করে বানিয়ে দিতে হবে। এবং নিচের মতো করে আউটপুট দিতে হবে। 
+
+var lastDay = 0;
+while(lastDay <= 60){
+    // console.log("medicine",lastDay);
+if( lastDay = lastDay + 3){
+    console.log('medicine',lastDay)
+}
+else{
+    console.log(lastDay)
+}
+    // if(lastDay / 3 = 0){
+    //     console.log('medicine')
+    // }
+    // else{
+    //     console.log('rest')
+    // }
+   
+    lastDay++;
+}
